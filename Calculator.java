f7785dafbce55c93ef6fe5427a1c15dcf400a4ef
justynaburzynska <burzynskajustyna@gmail.com>
2020-05-21 21:17:49 +0200

    unit

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main (String[] args) {
 
-        int result = createBMI(78, 2);
+        Parameters parameters = new Parameters();
+        parameters.setHeight(1.50d);
+        parameters.setWeight(60d);
+        parameters.setUnit(Unit.KG);
+
+        System.out.println(parameters.getHeight() + " " + parameters.getWeight());
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj jednostkę wagi");
+        String firstName = scan.nextLine();
+
+        if(firstName.equals("kg")){
+         parameters.setUnit(Unit.KG);
+        }
+
+        double result = createBMI(parameters);
         System.out.println(result);
         System.out.println(result);
     }
-    public static int createBMI(int weight, int height) {
-        return weight / (height * height);
+    public static double createBMI(Parameters parameters) {
+        if(Unit.KG.equals(parameters.getUnit())){
+            System.out.println("są to kilogramy");
+        } else {
+            System.out.println(("wsaga w IBS"));
+        }
+
+        return parameters.getWeight() / (parameters.getHeight() * parameters.getHeight());
     }
 }
--- Parameters.java
+++ Parameters.java
@@ -1,8 +1,30 @@
 public class Parameters {
    // public String units (String kg, String ibs) {
+    private double weight;
+    private double height;
+    private Unit unit;
 
-    public int weight;
-    public int height;
+    public Unit getUnit(){
+        return unit;
+    }
 
-    
+    public void setUnit(Unit unit){
+        this.unit = unit;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
