51de1c2d135c4b4c16229d96decea234797521f6
justynaburzynska <burzynskajustyna@gmail.com>
2020-05-21 20:15:47 +0200

    removed never used variable

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,6 @@
 public class Calculator {
 
     public static void main (String[] args) {
-        Parameters calculatorBMI = new Parameters();
 
         int result = createBMI(78, 2);
         System.out.println(result);
